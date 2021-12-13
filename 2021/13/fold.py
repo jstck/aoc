8c4ed1dcef942ef61f89f1989cf936c1a676038a
John Stäck <john.stack@gmail.com>
2021-12-13 08:42:18 +0100

    Solve 13

--- 2021/13/fold.py
+++ 2021/13/fold.py
@@ -52,3 +52,63 @@ for line in sys.stdin.readlines():
     folds.append((fold[0], int(fold[1])))
 
 vprint(1, folds)
+
+def foldX(s, c):
+    newset = set()
+    for (x, y) in s:
+        if x==c:
+            #No dots should be at fold, really
+            vprint(0, "DOT AT FOLD (X=%d): %d %d" % (c, x, y))
+            continue
+        if x>c:
+            x = 2*c-x
+
+        newset.add((x,y))
+
+    return newset
+
+
+def foldY(s, c):
+    newset = set()
+    for (x, y) in s:
+        if y==c:
+            #No dots should be at fold, really
+            vprint(0, "DOT AT FOLD (Y=%d): %d %d" % (c, x, y))
+            continue
+        if y>c:
+            y = 2*c-y
+
+        newset.add((x,y))
+    
+    return newset
+
+if part1:
+    (axis, c) = folds[0]
+
+    if axis == "x":
+        dots = foldX(dots, c)
+    else:
+        dots = foldY(dots, c)
+
+    vprint(1, dots)
+    print(len(dots), "dots")
+
+if part2:
+    for (axis, c) in folds:
+        if axis == "x":
+            dots = foldX(dots, c)
+        else:
+            dots = foldY(dots, c)
+        vprint(1, "Down to", len(dots), "dots")
+
+    xmax, ymax = 0, 0
+    for (x,y) in dots:
+        xmax = max(xmax, x)
+        ymax = max(ymax, y)
+
+    grid = [ ["."] * (xmax+1) for y in range(ymax+1)]
+    for (x, y) in dots:
+        grid[y][x] = "#"
+
+    for row in grid:
+        print("".join(row))
