dfd151d9bc4b439144d1aa1e99c00b67f1174128
John Stack <stack@spotify.com>
2022-12-01 19:59:51 +0100

    Add user agent

--- 2022/skapa.sh
+++ 2022/skapa.sh
@@ -3,20 +3,21 @@
 DAY=$1
 
 if [ -f "${DAY}/run.py" ]; then
-    echo "${DAY} already exists!"
+    echo "${DAY}/run.py already exists!"
     exit 1
 fi
 
 mkdir ${DAY}
 
-cp skeletor.py ${DAY}/run.py
+cp template.py ${DAY}/run.py
 
 DAY2=$(echo $DAY | sed 's/^0*//')
 
 COOKIE=$(cat cookies.txt)
 URL="https://adventofcode.com/2022/day/${DAY2}/input"
 
+USER_AGENT="https://github.com/jstck/aoc/blob/master/2022/skapa.sh by john.stack@gmail.com"
 
-curl -o ${DAY}/input.txt -b ${COOKIE} ${URL}
+curl -A "${USER_AGENT}" -o ${DAY}/input.txt -b ${COOKIE} ${URL}
 
 cd ${DAY}
