10dda77b8aaddf78430da4fb4418756284a011a9
John Stack <stack@spotify.com>
2023-12-02 23:10:48 +0100

    Fix template script

--- 2023/skapa.sh
+++ 2023/skapa.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+
 DAY=$(echo $1 | sed 's/^0*//')
 
-if [ -f "${DAY}/run.py" ]; then
-    echo "${DAY}/run.py already exists!"
+if [ $DAY -lt 10 ]
+then
+    DAY0=$(echo "0${DAY}")
+else
+    DAY0=$DAY
+fi
+
+
+if [ -f "${DAY0}/run.py" ]; then
+    echo "${DAY0}/run.py already exists!"
     exit 1
 fi
 
-mkdir ${DAY}
+mkdir ${DAY0}
 
-cp templates/aoc.py ${DAY}/run.py
-cp templates/part_one.py ${DAY}/part_one.py
-cp templates/part_two.py ${DAY}/part_two.py
-
+cp templates/*.py ${DAY0}/run.py
 
 COOKIE=$(cat cookies.txt)
 URL="https://adventofcode.com/2023/day/${DAY}/input"
 
 USER_AGENT="https://github.com/jstck/aoc/blob/master/2023/skapa.sh by john.stack@gmail.com"
 
-curl -A "${USER_AGENT}" -o ${DAY}/input.txt -b ${COOKIE} ${URL}
+curl -A "${USER_AGENT}" -o ${DAY0}/input.txt -b ${COOKIE} ${URL}
 
 cd ${DAY}
