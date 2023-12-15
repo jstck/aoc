47d8aa86115850dff39b68f14a44ba8a430adcd8
John Stack <stack@spotify.com>
2023-12-15 11:13:43 +0100

    Remove cruft

--- 2023/15/solution.py
+++ 2023/15/solution.py
@@ -19,13 +19,11 @@ sys.path.append("../..")
 from lib.aoc import *
 
 
-def harsh(s: str) -> int:
+def hash(s: str) -> int:
     value = 0
 
     for c in list(s):
-        value += ord(c)
-        value = value * 17
-        value = value % 256
+        value = (value + ord(c))*17%256
 
     return value
 
@@ -35,16 +33,9 @@ def part1(input: list[str]) -> int:
     total = 0
 
     for token in tokens:
-        total += harsh(token)
+        total += hash(token)
 
     return total
-
-def getbox(boxes, label):
-    i = harsh(label)
-
-    assert i<=255, f"Invalid hash/box: '{label}' -> {i}"
-
-    return boxes[i]
 
 def part2(input: list[str]) -> int:
 
@@ -52,14 +43,14 @@ def part2(input: list[str]) -> int:
     boxes = [ [] for _ in range(256)]
 
     ops = input[0].split(",")
-    
+
     for op in ops:
         if "=" in op:
             #Replace lens, or add new one at the end
             (newlabel, newval) = op.split("=")
             newval = int(newval)
 
-            box = getbox(boxes, newlabel)
+            box = boxes[hash(newlabel)]
 
             replaced = False
             for i, (label, _) in enumerate(box):
@@ -74,12 +65,13 @@ def part2(input: list[str]) -> int:
             #Remove a lens
             newlabel = op[:op.find("-")]
 
-            box = getbox(boxes, newlabel)
+            box = boxes[hash(newlabel)]
 
             for i, (label, _) in enumerate(box):
                 if newlabel==label:
                     del box[i]
                     break
+
 
     #Determine focusing power
     power = 0
