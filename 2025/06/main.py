e0a8721d468dcef06445f3dcf5218ca6f1ff7b12
John Stäck <john.stack@gmail.com>
2025-12-07 10:09:51 +0100

    Cleanup

--- 2025/06/main.py
+++ 2025/06/main.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 from functools import reduce
+from typing import Callable
 
 import sys
 
 
 sys.path.append("../..")
 from lib.aoc import *
+
+def operators(op) -> Callable[[int,int], int]:
+    if op=="+":
+        return lambda x,y: x+y
+    elif op=="*":
+        return lambda x,y: x*y
+    else:
+        assert False
+
 
 def part1(input: list[str]):
     numbers = [[int(x) for x in line.split()] for line in input[:-1]]
@@ -20,16 +30,9 @@ def part1(input: list[str]):
 
     for i in range(length):
         operands = [n[i] for n in numbers]
-        operator = operations[i]
-        print(operator, operands)
-        if operator=="+":
-            op = lambda x,y: x+y
-        elif operator=="*":
-            op = lambda x,y: x*y
-        else:
-            assert False
+        op = operations[i]
 
-        result = reduce(op, operands)
+        result = reduce(operators(op), operands)
 
         total1 += result
     return total1
@@ -37,35 +40,26 @@ def part1(input: list[str]):
 def part2(input: list[str]):
     total = 0
 
-    operators = input[-1]
+    operations = input[-1]
 
     #The leftmost digits of any problem operands are always in same column as operator
     #Step through and look for those
-    for i in range(len(operators)):
-        if operators[i]==" ": continue
+    for i in range(len(operations)):
+        if operations[i]==" ": continue
 
-        operator = operators[i]
-
-        if operator=="+":
-            op = lambda x,y: x+y
-        elif operator=="*":
-            op = lambda x,y: x*y
-        else:
-            assert False
+        op = operations[i]
 
         operands = []
         #Step through numbers in this column to build an operand, until there is one with all spaces
         for j in range(10): #Max sensible length, seems to actually be 4
             digits = "".join([line[i+j] for line in input[:-1]]).strip()
-            print(digits)
             if len(digits) == 0: #All spaces
                 break
             else:
                 operands.append(int(digits))
 
-        result = reduce(op, operands)
+        result = reduce(operators(op), operands)
         total += result
-
 
     return total
 
@@ -73,7 +67,6 @@ def part2(input: list[str]):
 if __name__ == "__main__":
     #Remove newlines, but make sure there's a safety whitespace at the end
     input = [line.rstrip("\n") + " " for line in sys.stdin.readlines() if len(line.strip())> 0]
-    print(input)
 
     p1 = part1(input)
     print("Part 1:", p1)
